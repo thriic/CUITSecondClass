64f34eccd8f4177d4c07f0346321871d0b3d7780
Thryanii <984524888@qq.com>
2023-05-29 20:12:06 +0800

    update

--- test.js
+++
@@ -1,6 +0,0 @@
-import SecondClass from "./src/secondClass.js";
-
-['s','ss','1******','1*'].forEach(element => {
-    if (element == undefined || element.includes('*')) return;
-    console.log(element)
-})
